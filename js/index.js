e30c3ba6e649b8a4ac09e217924e64aed737102d
Dmitry <boombapkilla@yandex.ru>
2020-10-09 15:10:38 +0300

    add burger menu

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ const sliderList = document.getElementById('slider-list')
 const sliderItem = document.querySelector('.slider__item')
 const arrowLeft = document.querySelector('.arrow--left')
 const arrowRight = document.querySelector('.arrow--right')
+const burger = document.getElementById('burger')
+const navbar = document.getElementById('navbar')
+const burgerCloseBtn = document.getElementById('burger-close')
 
 let sliderStep = 0
 let lastSlide = (sliderList.scrollWidth - sliderItem.clientWidth)
@@ -25,4 +28,33 @@ arrowLeft.addEventListener('click', () => {
 
     sliderList.style.transform = `translate(${sliderStep}px)`
 
+
 })
+burger.addEventListener('click', () => {
+ 
+    navbar.classList.add('navbar--burger')
+    document.body.classList.add('body--active-menu')
+    burger.style.display = 'none'
+
+    burgerCloseBtn.style.cssText = `
+    display: block;
+    position: absolute;
+    top: 2.5%;
+    right: 4.5%;
+    cursor: pointer;
+    z-index: 20;
+    `
+
+})
+burgerCloseBtn.addEventListener('click', () => {
+    navbar.classList.remove('navbar--burger')
+    document.body.classList.remove('body--active-menu')
+    burger.style.display = 'inline-flex'
+    burgerCloseBtn.style.display = 'none'
+})
+window.addEventListener('resize', () => {
+    if (innerWidth > 768) {
+        burger.style.display = 'none'
+    } else burger.style.display = 'inline-flex'
+})
+
