d86881cf44a1a80052abe78be487bcdf9f9bd714
Dmitry <boombapkilla@yandex.ru>
2020-10-11 13:16:52 +0300

    fix full screen menu

--- js/index.js
+++ js/index.js
@@ -2,9 +2,8 @@ const sliderList = document.getElementById('slider-list')
 const sliderItem = document.querySelector('.slider__item')
 const arrowLeft = document.querySelector('.arrow--left')
 const arrowRight = document.querySelector('.arrow--right')
-const burger = document.getElementById('burger')
 const navbar = document.getElementById('navbar')
-const burgerCloseBtn = document.getElementById('burger-close')
+
 
 let sliderStep = 0
 let lastSlide = (sliderList.scrollWidth - sliderItem.clientWidth)
@@ -30,30 +29,74 @@ arrowLeft.addEventListener('click', () => {
 
 
 })
-burger.addEventListener('click', () => {
 
-    navbar.classList.add('navbar--burger')
-    document.body.classList.add('body--active-menu')
-    burger.style.display = 'none'
+// window.addEventListener('resize', () => {
+//     if (innerWidth > 768) {
+//         burger.style.display = 'none'
+//     } else burger.style.display = 'block'
+// })
 
-    burgerCloseBtn.style.cssText = `
-    display: block;
-    position: fixed;
-    top: 2.5%;
-    right: 4.5%;
-    cursor: pointer;
-    z-index: 30;
-    `
+// burger.addEventListener('click', () => {
+//
+//     navbar.classList.add('navbar--burger')
+//     document.body.classList.add('body--active-menu')
+//     burger.style.display = 'none'
+//
+//     burgerCloseBtn.style.cssText = `
+//     display: block;
+//     position: fixed;
+//     top: 2.5%;
+//     right: 4.5%;
+//     cursor: pointer;
+//     z-index: 30;
+//     `
+//
+// })
+// burgerCloseBtn.addEventListener('click', () => {
+//     navbar.classList.remove('navbar--burger')
+//     document.body.classList.remove('body--active-menu')
+//     burger.style.display = 'inline-flex'
+//     burgerCloseBtn.style.display = 'none'
+// })
 
+
+// burgerLink.addEventListener('click', (event) => {
+//     event.preventDefault()
+//
+//     navbar.classList.remove('navbar--burger')
+//     document.body.classList.remove('body--active-menu')
+//     burger.style.display = 'inline-flex'
+//     burgerCloseBtn.style.display = 'none'
+// })
+// burgerLinks.forEach(link => {
+//     link.addEventListener('click', (event) => {
+//         event.preventDefault()
+//
+//         navbar.classList.remove('navbar--burger')
+//         document.body.classList.remove('body--active-menu')
+//         burger.style.display = 'inline-flex'
+//         burgerCloseBtn.style.display = 'none'
+//
+//     })
+// })
+
+
+let burger  = document.querySelector('.burger');
+let overlay = document.querySelector('.overlay');
+let body = document.querySelector('body');
+
+let links = document.querySelectorAll('.menu__link');
+
+links.forEach(function(element){
+    element.addEventListener('click' , toggleMenu);
 })
-burgerCloseBtn.addEventListener('click', () => {
-    navbar.classList.remove('navbar--burger')
-    document.body.classList.remove('body--active-menu')
-    burger.style.display = 'inline-flex'
-    burgerCloseBtn.style.display = 'none'
-})
-window.addEventListener('resize', () => {
-    if (innerWidth > 768) {
-        burger.style.display = 'none'
-    } else burger.style.display = 'inline-flex'
-})
+
+function toggleMenu(){
+    burger.classList.toggle('burger--active');
+    overlay.classList.toggle('overlay--active');
+    body.classList.toggle('body--active-menu');
+}
+
+burger.addEventListener('click' , toggleMenu);
+
+
